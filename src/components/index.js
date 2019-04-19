b0734324f0dad289875d18b8bc04fa098ee57206
chengwenxin <1260077156@qq.com>
2019-04-19 11:50:14 +0800

    feat:注册组件

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Header from './cwxHeader'
+import Footer from './cwxFooter'
+import SlideContent from './cwxSlideContent'
+Vue.component('cwx-header',Header)
+Vue.component('cwx-footer',Footer)
+Vue.component('cwx-slide-content',SlideContent)
