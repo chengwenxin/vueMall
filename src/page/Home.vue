1ed0663570c3302443089dde960b13c418a94316
chengwenxin <1260077156@qq.com>
2019-04-19 21:46:49 +0800

    调整首页样式

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -6,19 +6,17 @@
       </div>
       <div class="main">
         <div style="width:550px;height:300px;float:left;margin:0 25px;">
-          <el-carousel
-            trigger="click"
-            height="300px"
-            width="550px"
-          >
+          <el-carousel trigger="click" height="300px" width="550px">
             <el-carousel-item v-for="item in images" :key="item.src">
-              <router-link :to="item.path"> <img :src="item.src" /></router-link>
+              <router-link :to="item.path">
+                <img :src="item.src">
+              </router-link>
             </el-carousel-item>
           </el-carousel>
         </div>
-        <cwx-slide-content></cwx-slide-content>
-        <cwx-slide-content></cwx-slide-content>
-        <cwx-slide-content></cwx-slide-content>
+        <cwx-slide-content title="通知公告" path="/" :list="noticeList"></cwx-slide-content>
+        <cwx-slide-content title="工作动态" path="/" :list="noticeList"></cwx-slide-content>
+        <cwx-slide-content title="资助政策" path="/policy" :list="noticeList"></cwx-slide-content>
       </div>
       <div class="footer">
         <cwx-footer></cwx-footer>
@@ -27,15 +25,30 @@
   </div>
 </template>
 <script>
+import { getScholarList } from "../api/scholar.js";
 export default {
-  data:function(){
-    return {
-      images:[
-        {src:'../../static/1.jpg',path:'/goodslist'},
-        {src:'../../static/2.jpg',path:'/goodslist'},
-        {src:'../../static/3.jpg',path:'/goodslist'},
-      ]
+  mounted() {
+    this.getList();
+  },
+  methods: {
+    getList() {
+      getScholarList()
+        .then(data => {
+          this.noticeList = data.content || [];
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
+  },
+  data: function() {
+    return {
+      images: [
+        { src: "../../static/1.jpg", path: "/goodslist" },
+        { src: "../../static/2.jpg", path: "/goodslist" },
+        { src: "../../static/3.jpg", path: "/goodslist" }
+      ]
+    };
   }
 };
 </script>
@@ -54,5 +67,12 @@ export default {
       position: relative;
     }
   }
+}
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
 }
 </style>
