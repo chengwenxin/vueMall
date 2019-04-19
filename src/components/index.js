b611b64a665a54f61c49e1966066d586f1418203
chengwenxin <1260077156@qq.com>
2019-04-19 21:45:38 +0800

    注册组件

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import Header from './cwxHeader'
 import Footer from './cwxFooter'
 import SlideContent from './cwxSlideContent'
+import SlideContentItem from './cwxSlideContentItem'
+import SortMain from './cwxSortMain'
+import SortLeft from './cwxSortLeft'
+import SortRight from './cwxSortRight'
 Vue.component('cwx-header',Header)
 Vue.component('cwx-footer',Footer)
 Vue.component('cwx-slide-content',SlideContent)
+Vue.component('cwx-slide-content-item',SlideContentItem)
+Vue.component('cwx-sort-main',SortMain)
+Vue.component('cwx-sort-left',SortLeft)
+Vue.component('cwx-sort-right',SortRight)
