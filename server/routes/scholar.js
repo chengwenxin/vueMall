b321ebe858735f2808ec9d8d2e712551002ff0ad
chengwenxin <1260077156@qq.com>
2019-04-20 16:10:14 +0800

    后端增加通知公告、工作动态处理函数

--- server/routes/scholar.js
+++ server/routes/scholar.js
@@ -7,8 +7,9 @@ var $conf = require('../conf/conf');
 var pool = mysql.createPool($conf.mysql);
 router.use(express.static('public'));
 
-//开搞目录
-router.get('/',(req, res, next) => {
+//资助政策
+router.get('/policy',(req, res, next) => {
+
   let sql = `select * from policy`
   pool.query(sql, (err, result) => {
     if (err) {
@@ -25,4 +26,134 @@ router.get('/',(req, res, next) => {
     }
   })
 })
+
+//资助政策详情
+router.get('/policy/detail',(req, res, next) => {
+  let id = req.query.id
+  let sql = `select * from policy where id = ${id}`;
+  pool.query(sql, (err, result) => {
+    if(result && result.length>0){
+      result = result[0]
+    }
+    if (err) {
+      res.json({
+        status: '-1',
+        msg: err.message
+      });
+    } else {
+        res.json({
+          status: '1',
+          msg: '获取数据成功',
+          content: result
+        });
+    }
+  })
+})
+
+//通知公告
+router.get('/announcement',(req, res, next) => {
+
+  let sql = `select * from announcement`
+  pool.query(sql, (err, result) => {
+    if (err) {
+      res.json({
+        status: '-1',
+        msg: err.message
+      });
+    } else {
+        res.json({
+          status: '1',
+          msg: '获取数据成功',
+          content: result
+        });
+    }
+  })
+})
+
+//通知公告详情
+router.get('/announcement/detail',(req, res, next) => {
+  let id = req.query.id
+  let sql = `select * from announcement where id = ${id}`;
+  pool.query(sql, (err, result) => {
+    if(result && result.length>0){
+      result = result[0]
+    }
+    if (err) {
+      res.json({
+        status: '-1',
+        msg: err.message
+      });
+    } else {
+        res.json({
+          status: '1',
+          msg: '获取数据成功',
+          content: result
+        });
+    }
+  })
+})
+//工作动态
+router.get('/working',(req, res, next) => {
+
+  let sql = `select * from working`
+  pool.query(sql, (err, result) => {
+    if (err) {
+      res.json({
+        status: '-1',
+        msg: err.message
+      });
+    } else {
+        res.json({
+          status: '1',
+          msg: '获取数据成功',
+          content: result
+        });
+    }
+  })
+})
+
+//工作动态详情
+router.get('/working/detail',(req, res, next) => {
+  let id = req.query.id
+  let sql = `select * from working where id = ${id}`;
+  pool.query(sql, (err, result) => {
+    if(result && result.length>0){
+      result = result[0]
+    }
+    if (err) {
+      res.json({
+        status: '-1',
+        msg: err.message
+      });
+    } else {
+        res.json({
+          status: '1',
+          msg: '获取数据成功',
+          content: result
+        });
+    }
+  })
+})
+//阅读次数
+router.get('/readingtimes',(req, res, next) => {
+  let id = req.query.id
+  let reading_times = 0
+  let updatesql = ''
+  let sql = `select * from policy where id = ${id}`;
+  pool.query(sql, (err, result) => {
+   if(!err){
+    if(result && result.length>0){
+      result = result[0]
+    }
+    reading_times = result.reading_times + 1
+    updatesql =`update policy set reading_times= ${reading_times} where id =${id}`;
+    pool.query(updatesql,(err,result) => {
+      if(!err){
+        // console.log(result)
+      }
+    })
+   }
+  })
+})
+
 module.exports = router;
--- src/api/scholar.js
+++ src/api/scholar.js
@@ -1,5 +1,29 @@
 import http from './public'
-// 获取首页数据
+// 获取资助政策
 export const getScholarList = () => {
-  return http.fetchQuickSearch('/api/scholar/')
+  return http.fetchQuickSearch('/api/scholar/policy')
+}
+//获取资助政策详情
+export const getScholarDetail = (params) => {
+  return http.fetchGet('/api/scholar/policy/detail',params)
+}
+//统计文章阅读次数
+export const setReadingTimes = (params) => {
+  return http.fetchGet('/api/scholar/readingtimes',params)
+}
+// 获取通知公告
+export const getAnnouncementList = () => {
+  return http.fetchQuickSearch('/api/scholar/announcement')
+}
+//获取通知公告详情
+export const getAnnouncementListDetail = (params) => {
+  return http.fetchGet('/api/scholar/announcement/detail',params)
+}
+// 获取工作动态
+export const getWorkingtList = () => {
+  return http.fetchQuickSearch('/api/scholar/working')
+}
+//获取工作动态详情
+export const getWorkingtDetail = (params) => {
+  return http.fetchGet('/api/scholar/working/detail',params)
 }
