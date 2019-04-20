f72f014265d12463798fe4ecc0fd575d54b04e5b
chengwenxin <1260077156@qq.com>
2019-04-20 16:10:52 +0800

    增加资助政策详情页面

---
+++ src/page/PolicyDetail.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <div class="header">
+        <cwx-header></cwx-header>
+      </div>
+      <div class="main">
+        <cwx-sort-main title="学生资助"></cwx-sort-main>
+        <cwx-sort-left></cwx-sort-left>
+        <cwx-sort-right>
+          <cwx-article :detail="detail"></cwx-article>
+        </cwx-sort-right>
+      </div>
+      <div class="footer">
+        <cwx-footer></cwx-footer>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { getScholarDetail ,setReadingTimes} from "../api/scholar.js";
+export default {
+  mounted() {
+    this.ReadingTimes();
+    this.getList();
+  },
+  methods: {
+    getList() {
+      getScholarDetail({params:{id:this.id}})
+        .then(data => {
+          this.detail = data.content
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    //统计文章阅读次数
+    ReadingTimes(){
+        setReadingTimes({params:{id:this.id}}).then(()=>{}).catch(err=>{})
+    }
+  },
+  data: function() {
+    return {
+      detail:{},
+      list: [],
+      id:this.$route.params.id
+    };
+  }
+};
+</script>
+<style lang="scss">
+.home {
+  width: 100%;
+  .container {
+    padding: 0px;
+    width: 1200px;
+    .main {
+      height: 600px;
+    }
+    .header,
+    .main,
+    .footer {
+      position: relative;
+    }
+  }
+}
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
