a23d0eeb49a35b14fa9cf77011b31d65f9df294c
chengwenxin <1260077156@qq.com>
2019-04-20 16:09:41 +0800

    增加工作动态页面

---
+++ src/page/Working.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <div class="header">
+        <cwx-header></cwx-header>
+      </div>
+      <div class="main">
+        <cwx-sort-main title="工作动态"></cwx-sort-main>
+        <cwx-sort-left></cwx-sort-left>
+        <cwx-sort-right>
+          <cwx-slide-content-item paddingleft="padding-left:18px" :list="list"></cwx-slide-content-item>
+        </cwx-sort-right>
+      </div>
+      <div class="footer">
+        <cwx-footer></cwx-footer>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { getWorkingtList } from "../api/scholar.js";
+export default {
+  mounted() {
+    this.getList();
+  },
+  methods: {
+    getList() {
+      getWorkingtList()
+        .then(data => {
+          this.list = data.content || [];
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  },
+  data: function() {
+    return {
+      list: []
+    };
+  }
+};
+</script>
+<style lang="scss">
+.home {
+  width: 100%;
+  .container {
+    padding: 0px;
+    width: 1200px;
+    .main {
+      height: 600px;
+    }
+    .header,
+    .main,
+    .footer {
+      position: relative;
+    }
+  }
+}
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
